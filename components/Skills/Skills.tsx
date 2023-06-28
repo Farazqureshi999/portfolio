cba9094528c808bd22b55b01516512b9509e9481
farazqureshi999 <farazqureshi311@gmail.com>
2023-06-29 02:00:28 +0500

    first commit

---
+++ components/Skills/Skills.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+interface SkillsProps {
+  
+}
+
+const Skills: FC<SkillsProps> = () => {
+  return (
+    <div>
+     Skills
+    </div>
+  )
+}
+
+export default Skills;
---
+++ components/Work/Work.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+interface WorkProps {
+  
+}
+
+const Work: FC<WorkProps> = () => {
+  return (
+    <div>
+     Work
+    </div>
+  )
+}
+
+export default Work;
